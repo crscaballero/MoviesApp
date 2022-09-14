4b02f0ee5a0e38f91220fba3db398724c8018f9f
Cristian Caballero <48134692+crscaballero@users.noreply.github.com>
2022-09-13 20:50:28 -0700

    Init project

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import {useNavigation} from '@react-navigation/core';
+
+export const HomeScreen = () => {
+	// const navigation = useNavigation();
+
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
